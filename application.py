5f5a8bbe3ff0c1058e25eed0c194f3e71b3e962e
Fred-MabS <frederic.triquet@mabsilico.com>
2021-06-22 11:42:43 +0200

    json response

--- application.py
+++ application.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/<str:name>")
+def hello(name=None):
+    if not name:
+        message = jsonify(message='missing parameter')
+        return make_response(message, 400)
+    data = {'message': f'Hello {name}!'}
+    return jsonify(data)
