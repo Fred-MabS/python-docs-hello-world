06849ab1d670e363a688e79ea068c61c0063ded1
Fred-MabS <frederic.triquet@mabsilico.com>
2021-06-22 11:48:28 +0200

    fix api

--- application.py
+++ application.py
@@ -2,10 +2,13 @@ from flask import Flask, jsonify, make_response
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/<str:name>")
+@app.route("/<string:name>")
 def hello(name=None):
     if not name:
         message = jsonify(message='missing parameter')
         return make_response(message, 400)
     data = {'message': f'Hello {name}!'}
     return jsonify(data)
+
+if __name__ == '__main__':
+    app.run()
